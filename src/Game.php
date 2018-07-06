ba49647870c9244a7d90d6d9699d7e9f7c2f1d87
Valentin Khokhlov <mananathen@gmail.com>
2018-07-06 03:23:19 +0300

    del  blank lines

--- src/Game.php
+++ src/Game.php
@@ -12,7 +12,6 @@ function start($title, $get_question, $get_answer)
     line("Welcome to Brain Games!");
     line($title);
     line();
-
     $name = prompt("May I have your name?", '', ' ');
     line("Hello, %s", $name);
     line();
@@ -21,7 +20,6 @@ function start($title, $get_question, $get_answer)
     while ($score < VICTORY_SCORE) {
         $question = $get_question();
         $right_answer = $get_answer($question);
-
         line('Question: %s', $question);
         $user_answer = prompt("Your answer");
 
