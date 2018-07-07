4ddf838d47cbbcc4dd56bd34cb2d25cbb3484e4a
Valentin Khokhlov <mananathen@gmail.com>
2018-07-07 03:56:55 +0300

    task 9. add new game – brain-progression

---
+++ src/Games/Progression.php
@@ -0,0 +1,38 @@
+<?php
+namespace BrainGames\Games\Progression;
+
+const PROGRESSION_LENGTH = 10;
+const STEP_MIN = 1;
+const STEP_MAX = 10;
+const START_MIN = 0;
+const START_MAX = 100;
+const DESCRIPTION = 'What number is missing in this progression?';
+
+function init()
+{
+    $get_current_question_data = function () {
+        $start = rand(START_MIN, START_MAX);
+        $step = rand(STEP_MIN, STEP_MAX);
+        $seq = progression($start, $step, PROGRESSION_LENGTH);
+
+        $index_to_mark = rand(0, sizeof($seq) - 1);
+        $marked_seq = $seq;
+        $marked_seq[$index_to_mark] = '..';
+
+        return [
+            'question' => implode(' ', $marked_seq),
+            'answer' => strval($seq[$index_to_mark])
+        ];
+    };
+
+    \BrainGames\Game\start(DESCRIPTION, $get_current_question_data);
+}
+
+function progression($start, $step, $len)
+{
+    $seq = [];
+    for ($i = 0; $i < $len; $i += 1) {
+        $seq[] = $start + $i * $step;
+    }
+    return $seq;
+}
